16a8db7979cdcbf9cc737378ce16210152c48908
Ashley Hawkins <536669@escg.ac.uk>
2022-10-31 00:11:14 +0000

    begin procedural generation stuff

--- Assets/Scripts/Game/LoadTilemap.cs
+++ Assets/Scripts/Game/LoadTilemap.cs
@@ -23,7 +23,12 @@ public class LoadTilemap : MonoBehaviour
 {
     // Start is called before the first frame update
     Dictionary<TileGroup, Tilemap> maps;
-    public TileInfo[] tiles;
+    public TileInfo[] tileInfo;
+
+    Tile[] tiles;
+
+    public string loadFile;
+
     void Start()
     {
         maps = new Dictionary<TileGroup, Tilemap>
@@ -31,17 +36,51 @@ public class LoadTilemap : MonoBehaviour
             {TileGroup.ForegroundBasic, GameObject.Find("Grid/Tilemap_Foreground").GetComponent<Tilemap>()}
         };
 
-        Tile tile = ScriptableObject.CreateInstance<Tile>();
-        tile.name = tiles[0].name;
-        tile.sprite = tiles[0].sprite;
+        SetupTiles();
 
-        for (int i = 0; i < 20; i++)
-            maps[TileGroup.ForegroundBasic].SetTile(new Vector3Int(i - 10, -5, 0), tile);
+        WorldGen();
     }
 
-    // Update is called once per frame
+    void SetupTiles()
+    {
+        tiles = new Tile[tileInfo.Length];
+        for (int i = 0; i < tileInfo.Length; ++i)
+        {
+            tiles[i] = ScriptableObject.CreateInstance<Tile>();
+            tiles[i].name = tileInfo[i].name;
+            tiles[i].sprite = tileInfo[i].sprite;
+        }
+    }
+
+    float factor1 = 30;
+    float factor2 = 11;
+    public Vector2 seed = Vector2.zero;
+    void WorldGen()
+    {
+        if (factor1 == 0) factor1 = 1;
+        Vector2Int worldSize = new(100, 30);
+
+        for (int i = -worldSize.x; i <= worldSize.x; ++i)
+        {
+            print("i = " + i);
+            for (int j = 0; j < 10; ++j)
+            {
+                float x = i + j / 10f;
+                Debug.DrawRay(new Vector3(x, Mathf.PerlinNoise(seed.x + x / factor1, seed.y) * factor2  - factor2 / 2f), Vector3.up * 0.1f, Color.red);
+            }
+            float height2 = Mathf.PerlinNoise(seed.x + i / factor1, seed.y) * factor2 - factor2 / 2f;
+            print("Height: " + height2);
+            int height = Mathf.RoundToInt(height2);
+            maps[TileGroup.ForegroundBasic].SetTile(new Vector3Int(i, height), tiles[0]);
+            for (int j = height - 1; j >= -worldSize.y; --j)
+            {
+                maps[TileGroup.ForegroundBasic].SetTile(new Vector3Int(i, j), tiles[1]);
+            }
+        }
+    }
     void Update()
     {
-        
+        maps[TileGroup.ForegroundBasic].ClearAllTiles();
+        WorldGen();
     }
 }
--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -6,8 +6,10 @@ public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     public Animator animator;
+    Rigidbody2D rb;
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -34,6 +36,27 @@ public class Player : MonoBehaviour
         else if (walking)
         {
             animator.SetBool("Walking", false);
+        }
+
+        ProcessMovement();
+    }
+
+    void ProcessMovement()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector2.left * 1.5f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector2.right * 1.5f);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * 5.0f, ForceMode2D.Impulse);
         }
     }
 }
